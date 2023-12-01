0c2c7267604bdab6c708a2d935ce7065020de583
Yanis Breton <yanis.breton2@gmail.com>
2023-12-01 17:32:18 +0100

    Solve day one and initialize node

--- day01/secondStar.js
+++ day01/secondStar.js
@@ -1,6 +1,46 @@
 const { getLines } = require("../common/getLines");
 
-const regex = RegExp('([0-9])', 'g');
+const digitsAsString = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+
+function reverseString (stringToReverse) {
+  return stringToReverse
+    .split('')
+    .reverse()
+    .join('');
+}
+
+function buildRegex () {
+  const stringDigitsCapturingGroup = digitsAsString
+    .map((digitAsString) => `(${digitAsString})`);
+  const stringDigitsCapturingGroupReversed = digitsAsString
+    .map((digitAsString) => reverseString(digitAsString))
+    .map((digitAsString) => `(${digitAsString})`);
+  const allStringDigitsCapturingGroup = stringDigitsCapturingGroup.join('|');
+  const allStringDigitsCapturingGroupReversed = stringDigitsCapturingGroupReversed.join('|');
+  const stringRegex = `[0-9]|${allStringDigitsCapturingGroup}`;
+  const reversedStringRegex = `[0-9]|${allStringDigitsCapturingGroupReversed}`;
+  return {
+    normal: RegExp(stringRegex, 'g'),
+    reversed: RegExp(reversedStringRegex, 'g'),
+  };
+}
+
+const regex = buildRegex();
+
+function convertDigitFound (digit) {
+  const index = digitsAsString.findIndex((digitString) => digitString === digit);
+  return index < 0 ? digit : index + 1;
+}
 
 function main() {
   const lines = getLines(`${__dirname}/input`);
@@ -10,10 +50,12 @@ function main() {
   let sum = 0;
 
   for (const line of lines) {
-    const lineDigits = [...line.matchAll(/([0-9])/g)];
-    const firstDigit = lineDigits[0][0];
-    const lastDigit = lineDigits[lineDigits.length - 1][0];
+    const lineDigits = [...line.matchAll(regex.normal)];
+    const lineDigitsReversed = [...reverseString(line).matchAll(regex.reversed)];
+    const firstDigit = convertDigitFound(lineDigits[0][0]);
+    const lastDigit = convertDigitFound(reverseString(lineDigitsReversed[0][0]));
     const lineNumber = parseInt(`${firstDigit}${lastDigit}`, 10);
+    console.log(lineNumber);
     sum += lineNumber;
   }
 
